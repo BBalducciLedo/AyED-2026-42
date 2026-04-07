c69dfbaebfd4904eae60397a85a5534c2a556a43
BBalducciLedo <balducci.benicio2010@gmail.com>
2026-04-07 16:38:16 -0300

    subo mas cositas

---
+++ Etapa2/4_EstadoDeAgua/4_EstadoDeAgua/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_EstadoDeAgua
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese una temperatura en grados celcius: ");
+            int valor = int.Parse(Console.ReadLine());
+            if (valor <= 0)
+            {
+                Console.WriteLine("Se congela");
+            }
+            if (valor >= 100)
+            {
+                Console.WriteLine("Hierve");
+            }
+            if (valor >= 1 && valor <= 99)
+            {
+                Console.WriteLine("Está en estado líquido");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa2/5_Apocalipsis/5_Apocalipsis/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Apocalipsis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tenes comida? (SI/NO): ");
+            bool comida = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Tenes refugio? (SI/NO): ");
+            bool refugio = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Tenes un bate? (SI/NO): ");
+            bool bate = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Tenes agua? (SI/NO): ");
+            bool agua = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Tenes un botiquin? (SI/NO): ");
+            bool botiquin = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Tenes linterna? (SI/NO): ");
+            bool linterna = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Tenes mochila? (SI/NO): ");
+            bool mochila = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Tenes mechero? (SI/NO): ");
+            bool mechero = bool.Parse(Console.ReadLine());
+        }
+    }
+}
