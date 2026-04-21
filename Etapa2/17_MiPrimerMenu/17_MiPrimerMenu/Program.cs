f731c2a97fd2f9b25bc7c6935a0981d501d81a00
BBalducciLedo <balducci.benicio2010@gmail.com>
2026-04-21 16:27:26 -0300

    algo

---
+++ Etapa2/17_MiPrimerMenu/17_MiPrimerMenu/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _17_MiPrimerMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola, bienvenido al programa elige una de estas 3 opciones: 1)pizza, 2)pancho, 3)papitas 4)salir: ");
+            int opcion = int.Parse(Console.ReadLine());
+            while (opcion != 4)
+            {
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Te gusta la pizza");
+                        break;
+                    case 2:
+                        Console.WriteLine("Te gustan los panchos");
+                        break;
+                    case 3:
+                        Console.WriteLine("Te gusta las papitas");
+                        break;
+                    default:
+                        Console.WriteLine("Sos wachin");
+                        break;
+                }
+
+                opcion = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
