bdda80e232b22688d0829d0f6d04170df476a92d
BBalducciLedo <balducci.benicio2010@gmail.com>
2026-05-20 16:23:58 -0300

    chiqui tapia

---
+++ Etapa3/3_HotSale/3_HotSale/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_HotSale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la cantidad de productos comprados: ");
+            int[] productos = new int[int.Parse(Console.ReadLine())];
+            for (int i = 0; i < productos.Length; i++)
+            {
+                Console.WriteLine("Cuanto te salio el producto " + (i + 1) + "?: ");
+                productos[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(productos);
+            Console.WriteLine("El producto mas barato salio $" + productos[0]);
+            Console.WriteLine("El producto mas caro salio $" + productos[productos.Length - 1]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa3/4_PixelDreams/4_PixelDreams/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_PixelDreams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la cantidad de participantes: ");
+            int[] participantes = new int[int.Parse(Console.ReadLine())];
+            for (int i = 0; i < participantes.Length; i++)
+            {
+                Console.WriteLine("Cuantos puntos hizo el participante " + (i + 1) + "?: ");
+                participantes[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(participantes);
+            Array.Reverse(participantes);
+            for (int i = 0; i < participantes.Length; i++)
+            {
+                Console.WriteLine((i + 1) + "° lugar: " + participantes[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
